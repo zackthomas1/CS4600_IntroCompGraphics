a07c3f2bc8b675cc18bed0ddf8b12ccad912afef
zackthomas1 <zackthomas13@gmail.com>
2022-01-01 20:51:52 -0800

    Add Project 02 files. Add index.html to root directory.

---
+++ project02/app.js
@@ -0,0 +1,86 @@
+var uav = {
+		positionX : 0,
+		positionY : 0,
+		rotation  : 0,
+		scale     : 1,
+		altitude  : 0,
+	};
+var ground = {
+		positionX : 0,
+		positionY : 0,
+	};
+document.addEventListener("keydown", KeyDown, false);
+function KeyDown(e)
+{
+	var keyCode = e.key;
+	switch ( e.key ) {
+		case "w": case "ArrowUp"   : uav.scale    *= 1.01; break;
+		case "s": case "ArrowDown" : uav.scale    *= 0.99; break;
+		case "a": case "ArrowLeft" : uav.rotation -= 5;    break;
+		case "d": case "ArrowRight": uav.rotation += 5;    break;
+		case "q": case "PageUp"    : uav.altitude += 1; if ( uav.altitude > 100 ) uav.altitude = 100; break;
+		case "e": case "PageDown"  : uav.altitude -= 1; if ( uav.altitude <   0 ) uav.altitude =   0; break;
+		case "h":
+			var d = document.getElementById('controls');
+			d.style.display = d.style.display=="" ? "none" : "";
+			break;
+		case "F6":
+			document.getElementById('includedscript').remove();
+			var head = document.getElementsByTagName('head')[0];
+			var script = document.createElement('script');
+			script.src= 'project2.js';
+			script.id = 'includedscript';
+			head.appendChild(script);
+			console.log('New script loaded.');
+			break;
+	}
+	UpdateTrans();
+}
+function MoveUAV()
+{
+	uav.positionX = event.clientX;
+	uav.positionY = event.clientY;
+	UpdateTrans();
+}
+function UpdateTrans()
+{
+	var s = document.getElementById('shadow');
+	var a = uav.altitude * uav.scale;
+	s.style.transform = "translate(" + a + "px," + a + "px) translate(" + uav.positionX + "px," + uav.positionY + "px) rotate(" + uav.rotation + "deg) scale(" + uav.scale + ")";
+	s.style.filter = "blur(" + (uav.altitude*0.5) + "px)";
+	var m = GetTransform( uav.positionX, uav.positionY, uav.rotation, uav.scale );
+	var b = document.getElementById('uav');
+	b.style.transform = "matrix(" + m[0] + "," + m[1] + "," + m[3] + "," + m[4] + "," + m[6] + "," + m[7] + ")";
+	var offset = Array(
+		{ x:-51, y:-51 },
+		{ x: 51, y:-51 },
+		{ x:-51, y: 51 },
+		{ x: 51, y: 51 },
+	);
+	for ( var i=0; i<4; ++i ) {
+		var p = document.getElementById('propeller'+i);
+		var r = Math.random()*360;
+		var t = GetTransform( offset[i].x, offset[i].y, r, 1 );
+		t = ApplyTransform( t, m );
+		p.style.transform = "matrix(" + t[0] + "," + t[1] + "," + t[3] + "," + t[4] + "," + t[6] + "," + t[7] + ")";
+	}
+	var px = uav.positionX + ground.positionX * uav.scale;
+	var py = uav.positionY + ground.positionY * uav.scale;
+	document.body.style.backgroundPosition = px + "px " + py + "px";
+	document.body.style.backgroundSize = (uav.scale * 1600) + "px";
+}
+setInterval( function() {
+	var speed = uav.altitude * 0.25;
+	var angle = uav.rotation * Math.PI/180;
+	var velX  = -Math.sin(angle) * speed;
+	var velY  =  Math.cos(angle) * speed;
+	ground.positionX += velX;
+	ground.positionY += velY;
+	var sx = 1600;
+	var sy = sx;
+	if ( ground.positionX < 0  ) ground.positionX += sx;
+	if ( ground.positionY < 0  ) ground.positionY += sy;
+	if ( ground.positionX > sx ) ground.positionX -= sx;
+	if ( ground.positionY > sy ) ground.positionY -= sy;
+	UpdateTrans();
+}, 15 );
---
+++ project06/starter/project6.js
@@ -0,0 +1,100 @@
+var raytraceFS = `
+struct Ray {
+	vec3 pos;
+	vec3 dir;
+};
+
+struct Material {
+	vec3  k_d;	// diffuse coefficient
+	vec3  k_s;	// specular coefficient
+	float n;	// specular exponent
+};
+
+struct Sphere {
+	vec3     center;
+	float    radius;
+	Material mtl;
+};
+
+struct Light {
+	vec3 position;
+	vec3 intensity;
+};
+
+struct HitInfo {
+	float    t;
+	vec3     position;
+	vec3     normal;
+	Material mtl;
+};
+
+uniform Sphere spheres[ NUM_SPHERES ];
+uniform Light  lights [ NUM_LIGHTS  ];
+uniform samplerCube envMap;
+uniform int bounceLimit;
+
+bool IntersectRay( inout HitInfo hit, Ray ray );
+
+// Shades the given point and returns the computed color.
+vec3 Shade( Material mtl, vec3 position, vec3 normal, vec3 view )
+{
+	vec3 color = vec3(0,0,0);
+	for ( int i=0; i<NUM_LIGHTS; ++i ) {
+		// TO-DO: Check for shadows
+		// TO-DO: If not shadowed, perform shading using the Blinn model
+		color += mtl.k_d * lights[i].intensity;	// change this line
+	}
+	return color;
+}
+
+// Intersects the given ray with all spheres in the scene
+// and updates the given HitInfo using the information of the sphere
+// that first intersects with the ray.
+// Returns true if an intersection is found.
+bool IntersectRay( inout HitInfo hit, Ray ray )
+{
+	hit.t = 1e30;
+	bool foundHit = false;
+	for ( int i=0; i<NUM_SPHERES; ++i ) {
+		// TO-DO: Test for ray-sphere intersection
+		// TO-DO: If intersection is found, update the given HitInfo
+	}
+	return foundHit;
+}
+
+// Given a ray, returns the shaded color where the ray intersects a sphere.
+// If the ray does not hit a sphere, returns the environment color.
+vec4 RayTracer( Ray ray )
+{
+	HitInfo hit;
+	if ( IntersectRay( hit, ray ) ) {
+		vec3 view = normalize( -ray.dir );
+		vec3 clr = Shade( hit.mtl, hit.position, hit.normal, view );
+		
+		// Compute reflections
+		vec3 k_s = hit.mtl.k_s;
+		for ( int bounce=0; bounce<MAX_BOUNCES; ++bounce ) {
+			if ( bounce >= bounceLimit ) break;
+			if ( hit.mtl.k_s.r + hit.mtl.k_s.g + hit.mtl.k_s.b <= 0.0 ) break;
+			
+			Ray r;	// this is the reflection ray
+			HitInfo h;	// reflection hit info
+			
+			// TO-DO: Initialize the reflection ray
+			
+			if ( IntersectRay( h, r ) ) {
+				// TO-DO: Hit found, so shade the hit point
+				// TO-DO: Update the loop variables for tracing the next reflection ray
+			} else {
+				// The refleciton ray did not intersect with anything,
+				// so we are using the environment color
+				clr += k_s * textureCube( envMap, r.dir.xzy ).rgb;
+				break;	// no more reflections
+			}
+		}
+		return vec4( clr, 1 );	// return the accumulated color, including the reflections
+	} else {
+		return vec4( textureCube( envMap, ray.dir.xzy ).rgb, 0 );	// return the environment color
+	}
+}
+`;
